0a814c57d5eaff44dededfa3d56b8be6f4ca72d0
郑超 <1658955813@qq.com>
2021-04-10 23:50:49 +0800

    郑超ssm项目练习 进度：整合好项目框架，成功测试自动注入bean 时间：2021年4月10日

--- src/main/java/com/ssm/test/EmployeeMapperTest.java
+++ src/main/java/com/ssm/test/EmployeeMapperTest.java
@@ -1,24 +1,27 @@
-package dao;
+package com.ssm.test;
 
 import com.ssm.dao.DepartmentMapper;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  *
  * 注解：@ContextConfiguration指定spring配置文件的位置
  *
  */
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class EmployeeMapperTest {
 
-//    @Autowired
-//    DepartmentMapper departmentMapper;
+    @Autowired
+    DepartmentMapper departmentMapper;
 
     @Test
     public void test() {
-
+        System.out.println(departmentMapper);
     }
 
 }
